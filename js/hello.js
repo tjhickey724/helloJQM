3cee0db1c1e7af8f21e0b89b75fb7d043cbc1e47
tim <tjhickey@brandeis.edu>
2013-06-12 15:29:45 -0400

    went back to version 1.2 of jQueryMobile

--- js/hello.js
+++ js/hello.js
@@ -1,11 +1,17 @@
-$("#loginpage").on("pageshow", function(event){
-	alert("we just called pageshow");
-	var page = $("#index");
-	$("#status",page).html("hello there");
+$("#bmipage").live("pageshow", function(event){
+	/* alert("we just called pageshow");*/
+	var page = $("#bmipage");
+	$("#status",page).html("-----");
 })
 
-$("#loginpage").on("pageinit", function(){
-	alert("we just called pageinit");
-	var page = $("#index");
-	$("#status",page).html("we are running!");
+$("#bmipage").live("pageinit", function(){
+	/* alert("we just called pageinit");*/
+	var page = $("#bmipage");
+	$("#submit").live("click", function(e){
+		ht =$("#ht").val();
+		wt =$("#wt").val();
+	        alert("You typed in "+ht+" for ht and "+wt+" for wt");
+		bmi = 703*wt/(ht*ht);
+		$("#status",page).html("your BMI is "+bmi);
+        })
 })

7e09ac89f48438b063bd5c189bc4791f4c94c599
tim <tjhickey@brandeis.edu>
2013-06-12 16:04:37 -0400

    replace live with on

--- js/hello.js
+++ js/hello.js
@@ -1,15 +1,13 @@
 /* the page show event is called every time the page is loaded into DOM */
 $(document).on("pageshow","#bmipage", function(event){
-	/* alert("we just called pageshow");*/
 	var page = $("#bmipage");
 	$("#status",page).html("-----");
 })
 
 /* the pageinit event happens the first time the page is created */
 $(document).on("pageinit","#bmipage", function(event){
-	/* alert("we just called pageinit");*/
 	var page = $("#bmipage");
-	$("#submit").live("click", function(e){
+	$(document).on("click","#submit", function(e){
 		ht =$("#ht").val();
 		wt =$("#wt").val();
 	        alert("You typed in "+ht+" for ht and "+wt+" for wt");
